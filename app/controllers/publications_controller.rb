1139e5db4e86a98c987e2515efd2c9cc33260471
jhamile08 <jhamiledr222@gmail.com>
2024-12-12 09:03:14 -0500

    tailwind ready

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -38,7 +38,7 @@ class PublicationsController < ApplicationController
   def update
     respond_to do |format|
       if @publication.update(publication_params)
-        format.html { redirect_to @publication, notice: "Publication was successfully updated." }
+        format.html { redirect_to @publication, notice: t("publications.successfully_update") }
         format.json { render :show, status: :ok, location: @publication }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class PublicationsController < ApplicationController
     @publication.destroy!
 
     respond_to do |format|
-      format.html { redirect_to publications_path, status: :see_other, notice: "Publication was successfully destroyed." }
+      format.html { redirect_to publications_path, status: :see_other, notice: t("publications.successfully_destroyed") }
       format.json { head :no_content }
     end
   end
